6a14bccdbdea5a240299c17f15944fc312a46455
tzahie <tzahie.l@taboola.com>
2018-06-06 14:59:25 +0300

    re factoring sdk test page using data binding

--- app/src/main/java/com/taboola/taboolasample/ConfigFragment.java
+++ app/src/main/java/com/taboola/taboolasample/ConfigFragment.java
@@ -1,9 +1,9 @@
 package com.taboola.taboolasample;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +19,7 @@ import java.util.Map;
 public class ConfigFragment extends Fragment implements SettingsDialog.SettingsCallback {
     private TaboolaWidget mTaboolaWidget;
     private Map<String, String> mWidgetProperties = new HashMap<>();
+    private Properties mProperties = new Properties();
     private View mRootView;
 
     public ConfigFragment() {
@@ -26,25 +27,15 @@ public class ConfigFragment extends Fragment implements SettingsDialog.SettingsC
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_config, container, false);
-        Button reloadButton = (Button) mRootView.findViewById(R.id.reload_button);
-        Button settingsButton = (Button) mRootView.findViewById(R.id.settings_button);
-        mTaboolaWidget = (TaboolaWidget) mRootView.findViewById(R.id.taboola_widget);
+        Button reloadButton = mRootView.findViewById(R.id.reload_button);
+        Button settingsButton = mRootView.findViewById(R.id.settings_button);
+        mTaboolaWidget = mRootView.findViewById(R.id.taboola_widget);
 
-        reloadButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                reloadWidget();
-            }
-        });
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showSettingsDialog();
-            }
-        });
+        reloadButton.setOnClickListener(view -> reloadWidget());
+        settingsButton.setOnClickListener(view -> showSettingsDialog());
 
         setDefaultProperties();
         reloadWidget();
@@ -52,56 +43,45 @@ public class ConfigFragment extends Fragment implements SettingsDialog.SettingsC
         return mRootView;
     }
 
-    public void onSettingsSaved(Map<String, String> newSettings) {
-        mWidgetProperties = newSettings;
+    public void onSettingsSaved(Properties properties) {
+        mProperties = new Properties(properties);
         reloadWidget();
         Snackbar.make(mRootView, "Settings saved", Snackbar.LENGTH_LONG).show();
     }
 
     private void setDefaultProperties() {
-        mWidgetProperties.put(Const.PUBLISHER_KEY, "betterbytheminute-app");
-        mWidgetProperties.put(Const.MODE_KEY, "thumbnails-sdk3");
-        mWidgetProperties.put(Const.PLACEMENT_KEY, "Mobile");
-        mWidgetProperties.put(Const.PAGE_TYPE_KEY, "article");
-        mWidgetProperties.put(Const.TARGET_TYPE_KEY, "mix");
-        mWidgetProperties.put(Const.PAGE_URL_KEY, "http://www.example.com");
-        mWidgetProperties.put(Const.REFERRER_KEY, "http://www.example.com/ref");
-        mWidgetProperties.put(Const.ARTICLE_KEY, "");
-        mWidgetProperties.put(Const.SCROLL_ENABLED_KEY, "false");
-        mWidgetProperties.put(Const.AUTO_RESIZE_HEIGHT_KEY, "true");
-        mWidgetProperties.put(Const.ITEM_CLICK_ENABLED_KEY, "true");
+        mProperties.setPublisher("betterbytheminute-app");
+        mProperties.setMode("thumbnails-sdk3");
+        mProperties.setPlacement("Mobile");
+        mProperties.setPageType("article");
+        mProperties.setTargetType("mix");
+        mProperties.setPageUrl("http://www.example.com");
+        mProperties.setReferrer("http://www.example.com/ref");
+        mProperties.setShouldScroll(false);
+        mProperties.setShouldAutoResizeHeight(true);
     }
 
     private void reloadWidget() {
         mTaboolaWidget.reset();
 
-        mTaboolaWidget.setPublisher(mWidgetProperties.get(Const.PUBLISHER_KEY));
-        mTaboolaWidget.setMode(mWidgetProperties.get(Const.MODE_KEY));
-        mTaboolaWidget.setPlacement(mWidgetProperties.get(Const.PLACEMENT_KEY));
-        mTaboolaWidget.setPageType(mWidgetProperties.get(Const.PAGE_TYPE_KEY));
-        mTaboolaWidget.setPageUrl(mWidgetProperties.get(Const.PAGE_URL_KEY));
-        mTaboolaWidget.setTargetType(mWidgetProperties.get(Const.TARGET_TYPE_KEY));
-
-        boolean shouldScroll = Boolean.parseBoolean(mWidgetProperties.get(Const.SCROLL_ENABLED_KEY));
-        mTaboolaWidget.setScrollEnabled(shouldScroll);
-
-        boolean shouldResize = Boolean.parseBoolean(mWidgetProperties.get(Const.AUTO_RESIZE_HEIGHT_KEY));
-        mTaboolaWidget.setAutoResizeHeight(shouldResize);
-
-        boolean isClickEnabled = Boolean.parseBoolean(mWidgetProperties.get(Const.ITEM_CLICK_ENABLED_KEY));
-        mTaboolaWidget.setItemClickEnabled(isClickEnabled);
+        mTaboolaWidget.setPublisher(mProperties.getPublisher());
+        mTaboolaWidget.setMode(mProperties.getMode());
+        mTaboolaWidget.setPlacement(mProperties.getPlacement());
+        mTaboolaWidget.setPageType(mProperties.getPageType());
+        mTaboolaWidget.setPageUrl(mProperties.getPageUrl());
+        mTaboolaWidget.setTargetType(mProperties.getTargetType());
+        mTaboolaWidget.setScrollEnabled(mProperties.isShouldScroll());
+        mTaboolaWidget.setAutoResizeHeight(mProperties.isShouldAutoResizeHeight());
 
         HashMap<String, String> otherProperties = new HashMap<>();
-        otherProperties.put(Const.PAGE_URL_KEY, mWidgetProperties.get(Const.PAGE_URL_KEY));
-        otherProperties.put(Const.REFERRER_KEY, mWidgetProperties.get(Const.REFERRER_KEY));
+        otherProperties.put(Const.PAGE_URL_KEY, mProperties.getPageUrl());
+        otherProperties.put(Const.REFERRER_KEY, mProperties.getReferrer());
         mTaboolaWidget.pushCommands(otherProperties);
         mTaboolaWidget.setLogLevel(Logger.DEBUG);
         mTaboolaWidget.fetchContent();
     }
 
     private void showSettingsDialog() {
-        FragmentManager fm = getActivity().getSupportFragmentManager();
-        String tag = SettingsDialog.class.getSimpleName();
-        new SettingsDialog().showDialog(fm, tag, mWidgetProperties, this);
+        SettingsDialog.showSettingsDialog(getFragmentManager(), mProperties, this);
     }
 }
---
+++ app/src/main/java/com/taboola/taboolasample/Properties.java
@@ -0,0 +1,171 @@
+package com.taboola.taboolasample;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Properties implements Parcelable {
+
+    private String pageType;
+    private String pageUrl;
+    private String mode;
+    private String placement;
+    private String targetType;
+    private String publisher;
+    private String referrer;
+    private boolean shouldScroll;
+    private boolean shouldAutoResizeHeight;
+
+    public Properties() {
+    }
+
+    public Properties(String pageType, String pageUrl, String mode, String placement, String targetType, String publisher, String referrer,
+                      boolean shouldScroll, boolean autoResizeHeight) {
+        this.pageType = pageType;
+        this.pageUrl = pageUrl;
+        this.mode = mode;
+        this.placement = placement;
+        this.targetType = targetType;
+        this.publisher = publisher;
+        this.referrer = referrer;
+        this.shouldScroll = shouldScroll;
+        this.shouldAutoResizeHeight = autoResizeHeight;
+    }
+
+    public Properties(Properties other) {
+        this.pageType = other.pageType;
+        this.pageUrl = other.pageUrl;
+        this.mode = other.mode;
+        this.placement = other.placement;
+        this.targetType = other.targetType;
+        this.publisher = other.publisher;
+        this.referrer = other.referrer;
+        this.shouldScroll = other.shouldScroll;
+        this.shouldAutoResizeHeight = other.shouldAutoResizeHeight;
+    }
+
+    public String getPageType() {
+        return pageType;
+    }
+
+    public String getPageUrl() {
+        return pageUrl;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public String getPlacement() {
+        return placement;
+    }
+
+    public String getTargetType() {
+        return targetType;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getReferrer() {
+        return referrer;
+    }
+
+    public boolean isShouldScroll() {
+        return shouldScroll;
+    }
+
+    public boolean isShouldAutoResizeHeight() {
+        return shouldAutoResizeHeight;
+    }
+
+    public Properties setPageType(String pageType) {
+        this.pageType = pageType;
+        return this;
+    }
+
+    public Properties setPageUrl(String pageUrl) {
+        this.pageUrl = pageUrl;
+        return this;
+    }
+
+    public Properties setMode(String mode) {
+        this.mode = mode;
+        return this;
+    }
+
+    public Properties setPlacement(String placement) {
+        this.placement = placement;
+        return this;
+    }
+
+    public Properties setTargetType(String targetType) {
+        this.targetType = targetType;
+        return this;
+    }
+
+    public Properties setPublisher(String publisher) {
+        this.publisher = publisher;
+        return this;
+    }
+
+
+    public Properties setReferrer(String referrer) {
+        this.referrer = referrer;
+        return this;
+    }
+
+    public Properties setShouldScroll(boolean shouldScroll) {
+        this.shouldScroll = shouldScroll;
+        return this;
+    }
+
+    public Properties setShouldAutoResizeHeight(boolean shouldAutoResizeHeight) {
+        this.shouldAutoResizeHeight = shouldAutoResizeHeight;
+        return this;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.pageType);
+        dest.writeString(this.pageUrl);
+        dest.writeString(this.mode);
+        dest.writeString(this.placement);
+        dest.writeString(this.targetType);
+        dest.writeString(this.publisher);
+        dest.writeString(this.referrer);
+        dest.writeByte(this.shouldScroll ? (byte) 1 : (byte) 0);
+        dest.writeByte(this.shouldAutoResizeHeight ? (byte) 1 : (byte) 0);
+    }
+
+    protected Properties(Parcel in) {
+        this.pageType = in.readString();
+        this.pageUrl = in.readString();
+        this.mode = in.readString();
+        this.placement = in.readString();
+        this.targetType = in.readString();
+        this.publisher = in.readString();
+        this.referrer = in.readString();
+        this.shouldScroll = in.readByte() != 0;
+        this.shouldAutoResizeHeight = in.readByte() != 0;
+    }
+
+    public static final Parcelable.Creator<Properties> CREATOR = new Parcelable.Creator<Properties>() {
+        @Override
+        public Properties createFromParcel(Parcel source) {
+            return new Properties(source);
+        }
+
+        @Override
+        public Properties[] newArray(int size) {
+            return new Properties[size];
+        }
+    };
+}
