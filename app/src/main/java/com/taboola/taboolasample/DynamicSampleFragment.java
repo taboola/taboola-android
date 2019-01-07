77d5c570ed806c87db49d89001fcd88c9f9db09c
Tzahie Leibovich <Tzahie21@gmail.com>
2019-01-07 18:24:02 +0200

    Update DynamicSampleFragment.java

--- app/src/main/java/com/taboola/taboolasample/DynamicSampleFragment.java
+++ app/src/main/java/com/taboola/taboolasample/DynamicSampleFragment.java
@@ -1,6 +1,7 @@
 package com.taboola.taboolasample;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,9 +11,15 @@ import android.widget.LinearLayout;
 
 import com.taboola.android.TaboolaWidget;
 import com.taboola.android.listeners.TaboolaEventListener;
+import com.taboola.android.utils.Properties;
+import com.taboola.android.utils.SdkDetailsHelper;
+
+import java.util.HashMap;
 
 
 public class DynamicSampleFragment extends Fragment {
+
+    private TaboolaWidget mTaboolaView;
 
     public DynamicSampleFragment() {
         // Required empty public constructor
@@ -22,20 +29,20 @@ public class DynamicSampleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_dynamic_sample, container, false);
-        LinearLayout adContainer = (LinearLayout) rootView.findViewById(R.id.ad_container);
 
-        TaboolaWidget taboola = new TaboolaWidget(getContext());
-        taboola.setPublisher("betterbytheminute-app")
-                .setMode("thumbnails-sdk3")
-                .setPageType("article")
-                .setPageUrl("http://www.example.com")
-                .setPlacement("Mobile");
+
+        if (mTaboolaView == null) {
+            mTaboolaView = new TaboolaWidget(inflater.getContext());
+            buildTaboolaWidget(inflater.getContext(), mTaboolaView);
+        }
+
+        LinearLayout adContainer = (LinearLayout) rootView.findViewById(R.id.ad_container);
 
         // Optional. Set targetType only if it's specified by your Taboola account manager
         // taboola.setTargetType("<my-target-type>");
 
         // Optional - Setting the event listener to intercept clicks and/or size changes
-        taboola.setTaboolaEventListener(new TaboolaEventListener() {
+        mTaboolaView.setTaboolaEventListener(new TaboolaEventListener() {
             @Override
             public boolean taboolaViewItemClickHandler(String s, boolean b) {
                 return true;
@@ -46,9 +53,23 @@ public class DynamicSampleFragment extends Fragment {
             }
         });
 
-        adContainer.addView(taboola);
-        taboola.fetchContent();
+        adContainer.addView(mTaboolaView);
+        mTaboolaView.fetchContent();
 
         return rootView;
+    }
+
+    static void buildTaboolaWidget(Context context, TaboolaWidget taboolaWidget) {
+        int height = SdkDetailsHelper.getDisplayHeight(context);
+        ViewGroup.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height);
+        taboolaWidget.setLayoutParams(params);
+        taboolaWidget
+                .setPublisher("betterbytheminute-app")
+                .setMode("thumbnails-sdk3")
+                .setPageType("article")
+                .setPageUrl("http://www.example.com")
+                .setPlacement("Mobile");
+
+        taboolaWidget.setInterceptScroll(true);
     }
 }
